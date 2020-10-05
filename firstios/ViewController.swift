266997d8b852ccaf67cc171ee2b5720c7cab3bfe
sjk <tjswlsdl20@gmail.com>
2020-10-05 23:09:41 +0900

    first ios practice

---
+++ firstios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstios
+//
+//  Created by sjk on 2020/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
